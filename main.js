07718f2a4d7f03706def2d4275ae9e0528ceebd2
Your Name <you@example.com>
2023-10-19 17:09:37 +0500

    first

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const arr = [10, 100, 3, 4, 5,]
-const find =  (arr) => {
+const find = (arr) => {
     let max = arr[0]
-    let min  = arr[0]
+    let min = arr[0]
     for (let index = 0; index < arr.length; index++) {
         if (arr[index] > max) {
             max = arr[index]
@@ -17,3 +17,23 @@ const find =  (arr) => {
 }
 console.log(find(arr))
 
+
+function fn(...params) {
+    target = 'user'
+    for (let i = 0; i < params.length; i++) {
+        if (params[i] == target) {
+            console.log('ism bor');
+        }
+    }
+}
+fn('abduhamid', 'abd' , 'user' )
+
+
+let arr1 = [-1, 0, 2, 7]
+let arr2 = []
+for (let index = 0; index < arr1    .length; index++) {
+    if (arr[index] > 0) {
+        arr2.push(arr[index])
+    }
+}
+console.log(arr2)
